6179884f2035366a5f1053788416bb05560e41eb
tirumalakommi1234 <tirumalakommi1234.com>
2026-07-02 22:40:39 +0530

    HHH

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+p1=tiru # no space before and after equal ..here we are declaring the variables.
+p2=gouri
+
+
+echo "$p1 = Hi"
+echo "$p2 = Hello"
+echo "$p1 = how r u"
+echo "$p2 = fine .. how r u"
--- 02-variablesh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "A = Hi"
-echo "B = Hello"
-echo "A = how r u"
-echo "B = fine .. how r u"
