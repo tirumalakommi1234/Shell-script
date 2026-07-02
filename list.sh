28b3321ff56b6fa7a2cd77bdebfe8045275be665
tirumalakommi1234 <tirumalakommi1234.com>
2026-07-02 23:52:33 +0530

    sum

---
+++ list.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("RRR" "ABC" "DEF")
+
+echo "FIRST MOVIES IS: ${MOVIE[0]}"
+echo "SECOND MOVIES IS: ${MOVIE[1]}"
+echo "THIRD MOVIES IS: ${MOVIE[2]}"
+echo "ALL MOVIES IS:   ${MOVIE[@]}"
