3ed910c8cfb3a0c74be5eb1a5dd716c3cd0ff5c3
tirumalakommi1234 <tirumalakommi1234.com>
2026-07-02 22:47:33 +0530

    HHH

--- 03-vari.sh
+++ 03-vari.sh
@@ -4,7 +4,7 @@ p1=$1 # no space before and after equal ..here we are declaring the variables.
 p2=$2
 
 
-echo "$p1 = Hi"
-echo "${p2} = Hello"
+echo "$p1 = Hi $p2"
+echo "${p2} = Hello $p1"
 echo "${p1} = how r u"
 echo "$p2 = fine .. how r u"
