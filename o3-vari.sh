e3001b06b31b2788074dad719608dea989e6a085
tirumalakommi1234 <tirumalakommi1234.com>
2026-07-02 22:45:03 +0530

    HHH

---
+++ o3-vari.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+p1=$1 # no space before and after equal ..here we are declaring the variables.
+p2=$2
+
+
+echo "$p1 = Hi"
+echo "${p2} = Hello"
+echo "${p1} = how r u"
+echo "$p2 = fine .. how r u"
