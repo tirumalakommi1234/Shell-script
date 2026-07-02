73593fa18be351d54c7e95655d7fda3d1bbf99f8
tirumalakommi1234 <tirumalakommi1234.com>
2026-07-02 23:32:54 +0530

    HHH

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,7 +2,11 @@
 
 #echo "hi Gouri Tinnava?"
 
+#!/bin/bash
+
 num1=$1
 num2=$2
-sum=$(($num1+$num2))
-echo "sum of $num1 and $num2 is : $sum"
+
+sum=$((num1 + num2))
+
+echo "Sum of $num1 and $num2 is: $sum"
